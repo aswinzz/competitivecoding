4ff969eb7fedf079764c35ca590ae56684baac1f
aswinzz <aswinvb.aswin8@gmail.com>
2017-02-14 17:50:48 +0530

    Add files via upload

---
+++ HE-LR.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a[10000],t,n,i;
+    cin>>n>>t;
+    for(i=0;i<n;i++)
+        cin>>a[i];
+
+    for(i=t;i<n;i++)
+     cout<<a[i]<<" ";
+    for(i=0;i<t;i++)
+        cout<<a[i]<<" ";
+}
